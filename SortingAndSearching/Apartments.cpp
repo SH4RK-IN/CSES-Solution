39d8590cf53cc8ddf724ec5ba3e3d668fac55b3f
shan <shouryanbhardwaj68@gmail.com>
2025-08-22 19:34:50 +0530

    fix apartments.cpp

--- SortingAndSearching/Apartments.cpp
+++ SortingAndSearching/Apartments.cpp
@@ -3,49 +3,38 @@
 #include <algorithm>
 using namespace std;
 
-int result = 0;
-
-void search(const long long int& x, const long long int& y, vector<long long int>& V){
-    int l = 0;
-    int r = V.size() - 1;
-    for(l < r){
-        int mid = (l+r)/2;
-        if(x)
-    }
-}
-
-void check(const long long int& A, const long long int& K, vector<long long int>& V){
-    int small = A - K;
-    int large = A + K;
-
-
-}
-
 int main(){
-    long long int n,m,k;
-    cin >> n; //Applicant No.
-    cin >> m; //Apartment No.
-    cin >> k; //Allowed difference
-    
-    vector<long long int> a; //Applicants vector
-    for(int i {0}; i < n; i++){
-        int x;
-        cin >> x;
-        a.push_back(x);
-    }
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
 
-    vector<long long int> b; //Apartment vector
-    for(int i {0}; i < m; i++){
-        int x;
-        cin >> x;
-        b.push_back(x);
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
     }
-    
+    for(int i = 0; i < m; i++){
+        cin >> b[i];
+    }
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
 
-    for(int i = 0; i < n; i++){
-        check(a[i], k, b); // require, difference, appartment available vector
+    int ans = 0, i = 0, j = 0;
+    while(i < n && j < m){
+        if(b[j] > a[i] + k){
+            i++;
+        }
+        else if(b[j] < a[i] - k){
+            j++;
+        }
+        else{
+            i++;
+            j++;
+            ans++;
+        }
     }
-    cout << result;
+
+    cout << ans << endl;;
+
+    return 0;
 }
