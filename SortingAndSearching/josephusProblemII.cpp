2f3861145be53908c6addc4c65570aa6841aaee0
shan <shouryanbhardwaj68@gmail.com>
2025-09-11 21:30:18 +0530

    josephusProblemII

--- SortingAndSearching/josephusProblemII.cpp
+++ SortingAndSearching/josephusProblemII.cpp
@@ -1,3 +1,4 @@
+// TLE
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,7 +6,23 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    int n, k;
+    cin >> n >> k;
 
+    list<int> circle;
+    for(int i = 1; i <= n; i++) circle.push_back(i);
+
+    auto i = circle.begin();
+    while(!circle.empty()){
+        for(int j = 0; j < k; j++){
+            i++;
+            if(i == circle.end()) i = circle.begin();
+        }
+
+        cout << *i << " ";
+        i = circle.erase(i);
+        if(i == circle.end() && !circle.empty()) i = circle.begin();
+    }
+    
     return 0;
 }
