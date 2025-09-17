c50ba8c161467d8aefe72bc51c285e177fd4e51c
shan <shouryanbhardwaj68@gmail.com>
2025-09-17 17:00:49 +0530

    Add Subarray Divisibility

---
+++ SortingAndSearching/subarrayDivisibility.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N; cin >> N;
+    vector<int> sum(N);
+    int perfix_sum = 0;
+    sum[0] = 1;
+    for(int i = 0; i < N ; i++){
+        int temp; cin >> temp;
+        perfix_sum += temp;
+        sum[(perfix_sum % N + N) % N]++;
+    }
+
+    int ans = 0;
+    for(int i : sum){
+        ans += (i*(i-1))/2;
+    }   
+
+    cout << ans << endl;
+
+    return 0;
+}
